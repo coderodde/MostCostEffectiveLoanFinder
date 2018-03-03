bb8b6170153b9fe3284603a4c4d7800b6ef9ba92
Rodion Efremov <rodion.efremov@helsinki.fi>
2018-03-03 13:37:28 +0200

    Added more test.

--- src/test/java/net/coderodde/finance/loan/support/DefaultMostCostEffectiveLoanFinderTest.java
+++ src/test/java/net/coderodde/finance/loan/support/DefaultMostCostEffectiveLoanFinderTest.java
@@ -4,6 +4,7 @@ import net.coderodde.finance.loan.Actor;
 import net.coderodde.finance.loan.ActorGraph;
 import net.coderodde.finance.loan.MostCostEffectiveLoan;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import org.junit.Test;
 
 /**
@@ -77,4 +78,36 @@ public class DefaultMostCostEffectiveLoanFinderTest {
         assertEquals(actorB, loan.getDirectionMap().get(actorC));
         assertEquals(actorC, loan.getDirectionMap().get(actorD));
     }   
+    
+    @Test
+    public void test2() {
+        ActorGraph<String> graph = new ActorGraph<>();
+        Actor<String> actorA = new Actor<>("A");
+        Actor<String> actorB = new Actor<>("B");
+        Actor<String> actorC = new Actor<>("C");
+        Actor<String> actorD = new Actor<>("D");
+        
+        graph.addActor(actorA, 4.0);
+        graph.addActor(actorB, 10.0);
+        graph.addActor(actorC, 9.0);
+        graph.addActor(actorD, 8.0);
+        
+        graph.addArc(actorB, actorA, 0.4);
+        graph.addArc(actorC, actorA, 0.3);
+        graph.addArc(actorD, actorA, 0.2);
+        
+        DefaultMostCostEffectiveLoanFinder<String> finder = 
+                new DefaultMostCostEffectiveLoanFinder<>();
+        
+        MostCostEffectiveLoan<String> loan = 
+                finder.findLenders(actorA, 5.0, 0.5);
+        
+        assertEquals(5.0, loan.getReceivedPotential(), EPSILON);
+        assertEquals(5.0, loan.getRequestedPotential(), EPSILON);
+        assertEquals(actorA, loan.getDirectionMap().get(actorD));
+        assertNull(loan.getDirectionMap().get(actorB));
+        assertNull(loan.getDirectionMap().get(actorC));
+        
+        System.out.println(loan);
+    }
 }
